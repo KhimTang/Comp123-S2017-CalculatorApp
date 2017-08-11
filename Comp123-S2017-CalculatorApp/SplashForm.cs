faf62bb4c7f823ab0f18cf60a3b78bd27a6b333d
Khim Tang <ktang33@my.centennialcollege.ca>
2017-08-11 01:31:02 -0400

    Added SplashForm

---
+++ Comp123-S2017-CalculatorApp/SplashForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Khim Tang
+ * StudentID: 300920672
+ * Date: August 11. 2017
+ * Description: This is the SplashForm class
+ * Version: 0.1 - Created the SplashForm
+*/
+
+namespace Comp123_S2017_CalculatorApp
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
